349a00be6df639f57c67aa90e04e19e8a48224e9
Aditya Abhiram <khraos.genetor@gmail.com>
2024-05-09 11:28:11 +0530

    so I finally figured out that config = function() end breaks gen.nvim.

--- lua/plugins/gen.lua
+++ lua/plugins/gen.lua
@@ -1,11 +1,11 @@
 return {
 	"David-Kunz/gen.nvim",
 	opts = {
-		model = "phi3", -- The default model to use.
-		host = "localhost", -- The host running the Ollama service.
-		port = "11434", -- The port on which the Ollama service is listening.
-		quit_map = "q", -- set keymap for close the response window
-		retry_map = "<c-r>", -- set keymap to re-send the current prompt
+		model = "phi3",
+		host = "localhost",
+		port = "11434",
+		quit_map = "q",
+		retry_map = "<c-r>",
 		init = function(options)
 			pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
 		end,
@@ -18,15 +18,10 @@ return {
 				.. options.port
 				.. "/api/chat -d $body"
 		end,
-		-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-		-- This can also be a command string.
-		-- The executed command must return a JSON object with { response, context }
-		-- (context property is optional).
-		-- list_models = '<omitted lua function>', -- Retrieves a list of model names
-		display_mode = "float", -- The display mode. Can be "float" or "split".
-		show_prompt = false, -- Shows the prompt submitted to Ollama.
-		show_model = false, -- Displays which model you are using at the beginning of your chat session.
-		no_auto_close = false, -- Never closes the window automatically.
-		debug = false, -- Prints errors and the command which is run.
+		display_mode = "float",
+		show_prompt = true,
+		show_model = true,
+		no_auto_close = false,
+		debug = false,
 	},
-}
+} -- Custom Parameters (with defaults)
