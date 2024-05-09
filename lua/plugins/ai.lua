e1dc9db364874531b8dcfd6a0a13f9992be21a6e
Aditya Abhiram <khraos.genetor@gmail.com>
2024-05-09 09:07:39 +0530

    the issue is with the long lines of code.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,33 +1,3 @@
 return {
-	{
-		"Exafunction/codeium.vim",
-	},
-
-	{
-		"David-Kunz/gen.nvim",
-		opts = {
-			model = "phi3",
-			host = "localhost",
-			port = "11434",
-			quit_map = "q",
-			retry_map = "<c-r>",
-			init = function(options)
-				pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
-			end,
-			-- Function to initialize Ollama
-			command = function(options)
-				local body = { model = options.model, stream = true }
-				return "curl --silent --no-buffer -X POST http://"
-					.. options.host
-					.. ":"
-					.. options.port
-					.. "/api/chat -d $body"
-			end,
-			display_mode = "float",
-			show_prompt = true,
-			show_model = true,
-			no_auto_close = false,
-			debug = false,
-		},
-	},
+	"Exafunction/codeium.vim",
 }
