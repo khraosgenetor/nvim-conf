bec713d3337e66292c1100d5b912de5b3b165721
Aditya Abhiram <khraos.genetor@gmail.com>
2024-05-09 12:10:18 +0530

    the keybind SPC AI works now

--- init.lua
+++ init.lua
@@ -24,3 +24,4 @@ require("lazy").setup("plugins")
 
 vim.keymap.set("n", "<leader>wq", ":wqa<CR>", { desc = "Writes and Quits all Windows" })
 vim.keymap.set("n", "<leader>wa", ":wa<CR>", { desc = "Writes to all Windows" })
+vim.keymap.set({ 'n', 'v' }, '<leader>]', ':Gen<CR>')
