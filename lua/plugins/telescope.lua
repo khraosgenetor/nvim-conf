1c0236154e755ac0054e46e76eb81da1bae05069
Aditya Abhiram <khraos.genetor@gmail.com>
2024-05-17 20:15:17 +0530

    updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "v0.1.6", -- Note the correct tag syntax
+    tag = "0.1.6", -- Note the correct tag syntax
     dependencies = {
       "nvim-lua/plenary.nvim",
       "jvgrootveld/telescope-zoxide",
