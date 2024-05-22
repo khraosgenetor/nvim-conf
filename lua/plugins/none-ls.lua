844ca5193e7a5f6855f3f5b70820682e27894e62
Aditya Abhiram <khraos.genetor@gmail.com>
2024-05-22 15:40:25 +0530

    updates

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,10 +1,23 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "mfussenegger/nvim-lint",
+  },
   config = function()
     local null_ls = require("null-ls")
 
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        require("lint").try_lint()
+        require("lint").try_lint("biomejs")
+      end,
+    })
+
+
     null_ls.setup({
       sources = {
+        -- require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.asmfmt,
@@ -13,6 +26,11 @@ return {
         null_ls.builtins.formatting.google_java_format,
       },
     })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format your code. PS: Only works if formatting is setup." })
+    vim.keymap.set(
+      "n",
+      "<leader>gf",
+      vim.lsp.buf.format,
+      { desc = "Format your code. PS: Only works if formatting is setup." }
+    )
   end,
 }
